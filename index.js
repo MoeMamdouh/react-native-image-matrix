19a99cff7031ac394a50e30e9a63c49ed080e82e
Mohamed Mamdouh <mohamed.mamdouh@mowesolutions.com>
2017-03-30 12:11:53 +0200

    Handle Image Splitting

--- index.js
+++ index.js
@@ -1,6 +1,45 @@
-
-import { NativeModules } from 'react-native';
-
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  NativeModules,
+} from 'react-native';
 const { RNImageMatrix } = NativeModules;
+
+class RNImageMatrix extends Component {
+	constructor(props, context) {
+		super(props, context);
+		// this.passUrl().then(imagesMatrix => {
+			// console.log(imagesMatrix)
+			// this.props.getImagesMatrix = imagesMatrix;
+			// {() => this.props.getImagesMatrix(imagesMatrix)}()
+		// });
+		this.passPath();
+	}
+
+		async passPath() {
+			let imagePath   = this.props.imagePath;
+			let rows        = this.props.rows;
+			let columns     = this.props.columns;
+			 try {
+				const images = await  RNImageMatrix.getImageUrl(this.props.imagePath, rows, columns);
+				this.props.getImagesMatrix(images)
+				//return images;
+			} catch(e) {
+				console.error(e);
+			}
+		}
+
+		render() {
+			return (
+				<View style={{flex: 1}}>
+					<Text>Image is Downloaded Successfuly, Image Path : </Text>
+					<Text>{this.props.imagePath}</Text>
+				</View>
+			);
+		}
+		
+  }
 
 export default RNImageMatrix;
--- ios/RNImageMatrix.m
+++ ios/RNImageMatrix.m
@@ -1,5 +1,7 @@
 
 #import "RNImageMatrix.h"
+#import <React/RCTLog.h>
+#import <UIKit/UIKit.h>
 
 @implementation RNImageMatrix
 
@@ -8,6 +10,64 @@
     return dispatch_get_main_queue();
 }
 RCT_EXPORT_MODULE()
+RCT_EXPORT_METHOD(
+        getImageUrl:(NSString *)imageUrl
+        rows:(NSInteger *)rows
+        columns:(NSInteger *)columns
+        resolver: (RCTPromiseResolveBlock)resolve
+        rejecter:(RCTPromiseRejectBlock)reject) {
+    RCTLogInfo(@"Image URL %@ at", imageUrl);
+    NSLog(@"sequence = %i", rows);
+    NSURL* fileURL = [[NSURL alloc] initFileURLWithPath:imageUrl];
+    UIImage *image = [UIImage imageWithContentsOfFile:imageUrl];
+    
+    NSMutableArray *images = [self splitImage:image intoGridOfImagesWithRows:rows columns:columns];
+    NSMutableArray *imagesPaths = [NSMutableArray new];
+    int count = 0;
+    for(UIImage *image in images) {
+        NSString *imagePath = [[self documentPath] stringByAppendingPathComponent:[NSString stringWithFormat:@"%d.jpg", count]];
+        NSData *imageData = UIImageJPEGRepresentation(image, 1.0);
+        [imageData writeToFile:imagePath atomically:YES];
+        [imagesPaths addObject:imagePath];
+        count++;
+    }
+    resolve(imagesPaths);
+}
 
+- (NSString *)documentPath
+{
+    NSArray * paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    return ([paths count] > 0) ? [paths objectAtIndex:0] : nil;
+}
+
+- (NSMutableArray *)splitImage:(UIImage *)image intoGridOfImagesWithRows:(NSInteger)numberOfRows columns:(NSInteger)numberOfColumns
+{
+    CGFloat imageWidth = image.size.width * image.scale;
+    CGFloat imageHeight = image.size.height * image.scale;
+    CGSize imageSize = CGSizeMake(imageWidth, imageHeight);
+    
+    NSMutableArray *images = [NSMutableArray new];
+    
+    
+    CGFloat xPos = 0.0, yPos = 0.0;
+    CGFloat width = imageSize.width / numberOfColumns;
+    CGFloat height = imageSize.height / numberOfRows;
+    
+    for (int y = 0; y < numberOfRows; y++) {
+        xPos = 0.0;
+        for (int x = 0; x < numberOfColumns; x++) {
+            
+            CGRect rect = CGRectMake(xPos, yPos, width, height);
+            CGImageRef cImage = CGImageCreateWithImageInRect([image CGImage],  rect);
+            
+            UIImage *dImage = [[UIImage alloc] initWithCGImage:cImage];
+            [images addObject:dImage];
+            xPos += width;
+        }
+        yPos += height;
+    }
+    
+    return images;
+}
 @end
   
