cae962d204a32561c6e5a8aad6ce399875dd339f
Mohamed Mamdouh <mohamed.mamdouh@mowesolutions.com>
2017-03-30 13:42:31 +0200

    Fix class name

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import {
 } from 'react-native';
 const { RNImageMatrix } = NativeModules;
 
-class RNImageMatrix extends Component {
+class ImageMatrix extends Component {
 	constructor(props, context) {
 		super(props, context);
 		// this.passUrl().then(imagesMatrix => {
@@ -18,28 +18,28 @@ class RNImageMatrix extends Component {
 		this.passPath();
 	}
 
-		async passPath() {
-			let imagePath   = this.props.imagePath;
-			let rows        = this.props.rows;
-			let columns     = this.props.columns;
-			 try {
-				const images = await  RNImageMatrix.getImageUrl(this.props.imagePath, rows, columns);
-				this.props.getImagesMatrix(images)
-				//return images;
-			} catch(e) {
-				console.error(e);
-			}
-		}
+    async passPath() {
+        let imagePath   = this.props.imagePath;
+        let rows        = this.props.rows;
+        let columns     = this.props.columns;
+            try {
+            const images = await RNImageMatrix.getImageUrl(this.props.imagePath, rows, columns);
+            console.log('images ', images)
+            this.props.getImagesMatrix(images)
+            //return images;
+        } catch(e) {
+            console.error(e);
+        }
+    }
 
-		render() {
-			return (
-				<View style={{flex: 1}}>
-					<Text>Image is Downloaded Successfuly, Image Path : </Text>
-					<Text>{this.props.imagePath}</Text>
-				</View>
-			);
-		}
-		
+    render() {
+        return (
+            <View style={{flex: 1}}>
+                <Text>Image is Downloaded Successfuly, Image Path : </Text>
+                <Text>{this.props.imagePath}</Text>
+            </View>
+        );
+    }
   }
 
-export default RNImageMatrix;
+export default ImageMatrix;
